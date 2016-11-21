912d8339191acd618f9d106e59b06d5aba2b897e
yzc <yzc@git.com>
2016-11-21 09:04:45 +0800

    add xcode

---
+++ GitDemo/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  GitDemo
+//
+//  Created by 姚祚成 on 16/11/21.
+//  Copyright © 2016年 EMYZC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let a : String = "hello my git"
+        print("git demo " + a)
+        
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
