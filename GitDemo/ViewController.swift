abf8e234158aab6946b6ff830b18774f4813e938
yzc <yzc@git.com>
2016-11-21 09:10:24 +0800

    add view

--- GitDemo/ViewController.swift
+++ GitDemo/ViewController.swift
@@ -16,6 +16,11 @@ class ViewController: UIViewController {
         let a : String = "hello my git"
         print("git demo " + a)
         
+        
+        let view = UIView.init(frame: CGRect.init(x: 0, y: 0, width: 100, height: 100))
+        view.backgroundColor = UIColor.red
+        self.view.addSubview(view)
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
